0d40f7d0d6c424e41c605fd13c27211d84f353c3
PieroLopez12 <pedrolopez@upeu.edu.pe>
2022-01-25 11:57:31 -0500

    segundo Avance

---
+++ database/migrations/2022_01_25_154249_create_clientes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',255);
+            $table->text('direccion',250);
+            $table->string('celular',9);
+            $table->string('num_doc',15);
+            $table->string('estado',[1,0])->default('1');
+            $table->unsignedBigInteger('tipo_cliente_id');
+            $table->foreign('tipo_cliente_id')->references('id')->on('tipo_cliente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
---
+++ database/migrations/2022_01_25_162439_create_usuarios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombres',90);
+            $table->string('apellidos',150);
+            $table->string('celular',9);
+            $table->string('direccion',90);
+            $table->string('user',50);
+            $table->string('password',100);
+            $table->string('estado',[1,0])->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
